81ece5717ee03504a8cbc4c968e68ebf1b6bf4eb
Chris Laprun <metacosm@gmail.com>
2019-12-13 21:17:50 +0100

    feat: initial re-implementation using go-plugin instead of pie

--- capability/plugin.go
+++ capability/plugin.go
@@ -1,10 +1,12 @@
 package capability
 
 import (
+	"github.com/hashicorp/go-plugin"
 	halkyon "halkyon.io/api/capability/v1beta1"
 	framework "halkyon.io/operator-framework"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"net/rpc"
 )
 
 type Plugin interface {
@@ -13,6 +15,20 @@ type Plugin interface {
 	GetType() halkyon.CapabilityType
 	GetWatchedResourcesTypes() []schema.GroupVersionKind
 	ReadyFor(owner framework.Resource) framework.DependentResource
+}
+
+var _ plugin.Plugin = &GoPluginPlugin{}
+
+type GoPluginPlugin struct {
+	Delegate PluginResource
+}
+
+func (p *GoPluginPlugin) Server(b *plugin.MuxBroker) (interface{}, error) {
+	return NewPluginServer(p.Delegate), nil
+}
+
+func (p *GoPluginPlugin) Client(b *plugin.MuxBroker, client *rpc.Client) (interface{}, error) {
+	return &PluginClient{client: client}, nil
 }
 
 type PluginRequest struct {
@@ -38,4 +54,18 @@ type BuildResponse struct {
 type PluginResource interface {
 	framework.DependentResource
 	SetOwner(owner framework.Resource)
+	GetSupportedCategory() halkyon.CapabilityCategory
+	GetSupportedType() halkyon.CapabilityType
+}
+
+type TypeRegistry map[halkyon.CapabilityType]bool
+type CategoryRegistry map[halkyon.CapabilityCategory]TypeRegistry
+
+var SupportedCategories CategoryRegistry
+var Plugins []Plugin
+
+var Handshake = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "HALKYON_CAPABILITY_PLUGIN",
+	MagicCookieValue: "io.halkyon.capability.plugin",
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module halkyon.io/plugins
 go 1.13
 
 require (
-	github.com/natefinch/pie v0.0.0-20170715172608-9a0d72014007
+	github.com/hashicorp/go-plugin v1.0.2-0.20191004171845-809113480b55
 	halkyon.io/api v1.0.0-beta.7
 	halkyon.io/operator-framework v0.0.0-20191108175501-3d0a053bc383
 	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
 )
+
+replace halkyon.io/operator-framework => ../operator-framework
