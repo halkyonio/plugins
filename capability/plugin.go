7632fd11d631825e19560bc36d00263f1a7380f0
Chris Laprun <metacosm@gmail.com>
2020-01-14 17:23:56 +0100

    refactor: move PluginResource to own file

--- capability/plugin.go
+++ capability/plugin.go
@@ -77,69 +77,6 @@ type BuildResponse struct {
 	Built runtime.Object
 }
 
-type PluginResource interface {
-	GetSupportedCategory() halkyon.CapabilityCategory
-	GetSupportedTypes() []halkyon.CapabilityType
-	GetDependentResourcesWith(owner v1beta1.HalkyonResource) []framework.DependentResource
-}
-
-type SimplePluginResourceStem struct {
-	ct []halkyon.CapabilityType
-	cc halkyon.CapabilityCategory
-}
-
-func NewSimplePluginResourceStem(cat halkyon.CapabilityCategory, typ halkyon.CapabilityType) SimplePluginResourceStem {
-	return SimplePluginResourceStem{cc: cat, ct: []halkyon.CapabilityType{typ}}
-}
-func (p SimplePluginResourceStem) GetSupportedCategory() halkyon.CapabilityCategory {
-	return p.cc
-}
-
-func (p SimplePluginResourceStem) GetSupportedTypes() []halkyon.CapabilityType {
-	return p.ct
-}
-
-type AggregatePluginResource struct {
-	category        halkyon.CapabilityCategory
-	pluginResources map[halkyon.CapabilityType]PluginResource
-}
-
-func NewAggregatePluginResource(resources ...PluginResource) (PluginResource, error) {
-	apr := AggregatePluginResource{
-		pluginResources: make(map[halkyon.CapabilityType]PluginResource, len(resources)),
-	}
-	for _, resource := range resources {
-		category := categoryKey(resource.GetSupportedCategory())
-		if len(apr.category) == 0 {
-			apr.category = category
-		}
-		if !apr.category.Equals(category) {
-			return nil, fmt.Errorf("can only aggregate PluginResources providing the same category, got %v and %v", apr.category, category)
-		}
-		for _, capabilityType := range resource.GetSupportedTypes() {
-			apr.pluginResources[typeKey(capabilityType)] = resource
-		}
-	}
-	return apr, nil
-}
-
-func (a AggregatePluginResource) GetSupportedCategory() halkyon.CapabilityCategory {
-	return a.category
-}
-
-func (a AggregatePluginResource) GetSupportedTypes() []halkyon.CapabilityType {
-	types := make([]halkyon.CapabilityType, 0, len(a.pluginResources))
-	for capabilityType := range a.pluginResources {
-		types = append(types, capabilityType)
-	}
-	return types
-}
-
-func (a AggregatePluginResource) GetDependentResourcesWith(owner v1beta1.HalkyonResource) []framework.DependentResource {
-	capType := typeKey(owner.(*halkyon.Capability).Spec.Type)
-	return a.pluginResources[capType].GetDependentResourcesWith(owner)
-}
-
 type typeRegistry map[halkyon.CapabilityType]Plugin
 type pluginsRegistry map[halkyon.CapabilityCategory]typeRegistry
 
